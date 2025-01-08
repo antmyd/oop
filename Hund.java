b60aa9417435117049337d6977e9a28937938233
antmyd <antonmydlach@gmail.com>
2025-01-08 22:41:39 +0100

    beginne mit oop

--- Hund.java
+++ Hund.java
@@ -3,6 +3,10 @@ public class Hund {
     String name;
     int alter;
 
+    public Hund(String neuerName, int neuesAlter) {
+    this.name = neuerName;
+    this.alter = neuesAlter;
+}
     //Methoden
     public void bellen() {
         System.out.println("wuff wuff");
--- Main.java
+++ Main.java
@@ -2,6 +2,15 @@ public class Main {
     public static void main(String[] args) {
         //um oop zu verstehen: muss mna Klassen, Objekte, Konstruktoren verstehen
         //oop=programmierstil
-        Hund objektVonTypHund = new Hund();
+        
+    
+       //Konstruktor
+       Hund objektVonTypHund = new Hund("Bello", 7);
+       System.out.println(objektVonTypHund.name);
+       System.out.println(objektVonTypHund.alter);
+
+       Hund objektVonTypHund2 = new Hund("Mars", 6);
+       System.out.println(objektVonTypHund2.name);
+       System.out.println(objektVonTypHund2.alter);
     }
 }
