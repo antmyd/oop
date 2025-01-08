fe4a0becdb020dd17474eba3ff60e026c76feb23
antmyd <antonmydlach@gmail.com>
2025-01-08 22:02:01 +0100

    ich starte mit oop

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //um oop zu verstehen: muss mna Klassen, Objekte, Konstruktoren verstehen
+        //oop=programmierstil
+        Hund objektVonTypHund = new Hund();
+    }
+}
